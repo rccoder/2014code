c6dc1a6aab6b1f05cca21f5211ae1c3bbac72830
rccoder <admin@rccoder.net>
2014-11-08 19:22:03 +0800

    三个线索化基本完成

--- DataStruction/Project2/main.cpp
+++ DataStruction/Project2/main.cpp
@@ -40,7 +40,7 @@ BTtree * preCreateTree()
         BT -> rtag = false;
         cout << "输入" << ch << "的左子树" << endl;
         BT -> lchild = preCreateTree();
-        cout << "输入" << ch << "的右字数" << endl;
+        cout << "输入" << ch << "的右子树" << endl;
         BT -> rchild = preCreateTree();
     }
     return BT;
@@ -168,6 +168,7 @@ void listTreeBehNoRe(BTtree * BT)
     }
 
 }
+//遍历操作
 void listTree(BTtree * BT)
 {
     cout << "=递归实现：=" << endl << endl;
@@ -212,9 +213,11 @@ void printWithList(BTtree * BT)
         }
     }
 }
+//按广义表输出，此函数主要用来给广义表的头尾添加括号
 void printList(BTtree *BT)
 {
     BTtree *tep = BT;
+    //初始化i
     i = 0;
     withList[i] = '(';
     i++;
@@ -224,7 +227,7 @@ void printList(BTtree *BT)
     withList[i] = '\0';
 }
 /*
-*
+*按节点数建立相应二叉树
 */
 BTtree * levCreateTree()
 {
@@ -266,28 +269,33 @@ BTtree * levCreateTree()
     return BT;
 }
 /*
-*中序二叉树线索化
+*中序二叉树线索化辅助函数
 */
 void miding(BTtree p)
 {
     if(p != NULL)
     {
         miding(p -> lchild);
-        if(!p -> lchild)
+        //左子树线索化
+        if(p -> lchild == NULL)
         {
             p -> lchild = pre;
             p -> ltag = true;
         }
-        if(!pre ->rchild)
+        //右子树线索化
+        if(pre ->rchild == NULL)
         {
-            p -> rchild = pre;
+            pre -> rchild = pre;
             pre -> rtag = true;
         }
         pre = p;
         midding(p -> rchild);
     }
 }
-bool toTreeListMid(BTtree * BT, BTtree &HEAD)
+/*
+*中序线索化的起始函数
+*/
+bool toTreeListMid(BTtree  BT, BTtree &HEAD)
 {
     miding(BTtree p);
     HEAD = new BTtree;
@@ -309,6 +317,7 @@ bool toTreeListMid(BTtree * BT, BTtree &HEAD)
     }
     return 1;
 }
+//中序线索化的输出
 void toTreeListMidPrint(BTtree HEAD)
 {
     BTtree p = HEAD -> lchild;
@@ -327,16 +336,256 @@ void toTreeListMidPrint(BTtree HEAD)
         p = p -> rchild;
     }
 }
+/*
+*前序线索二叉树辅助函数
+*/
+void preing(BTtree p)
+{
+    if(p != NULL)
+    {
+        //前驱线索化
+        if(p -> lchild ==NULL)
+        {
+            p -> lchild = pre;
+            p -> ltag = true;
+        }
+        //后继线索化
+        if(pre -> rchild == NULL)
+        {
+            pre -> rchild = p;
+            pre ->rtag = true;
+        }
+        pre = p;
+        //左右子树线索化
+        if(p ->ltag == false)
+        {
+            preing(p -> lchild);
+        }
+        if(p -> rtag == false)
+        {
+            preing(p -> rchild);
+        }
+    }   
+}
+/*
+*前序线索化二叉树的起始函数
+*/
+bool toTreeListPre(BTtree BT, BTtree &HEAD)
+{
+    preing(BTtree p);
+    HEAD  = new BTtree;
+    HEAD -> ltag = false;
+    HEAD -> rtag = true;
+    HEAD -> rchild = HEAD;
+    if(BT == NULL)
+    {
+        HEAD -> lchild = HEAD;
+    }
+    else
+    {
+        HEAD -> lchild  = BT;
+        pre  = HEAD;
+        preing(BT);
+        pre -> rchild  = HEAD;
+        pre -> rtag  = true;
+        HEAD -> rchild = pre;
+    }
+    return 1;
+}
+/*
+*前序线索化二叉树的输出函数
+*/
+void toTreeListPrePrint(BTtree HEAD)
+{
+    BTtree  p = HEAD -> lchild;
+    while(p != HEAD)
+    {
+        cout << p -> data << "->";
+        while(p -> ltag == false)
+        {
+            p = p -> lchild;
+            cout << p -> data << "->";
+        }
+        while(p -> rtag == true && p -> rtag != HEAD)
+        {
+            p = p -> rchild;
+            cout <<　p -> data << "->";
+        }
+        if(p -> ltag == false)
+        {
+            p = p -> lchild;
+        }
+        else
+        {
+            p = p -> rchild;
+        }
+    }
+}
+/*
+*后序线索化辅助函数
+*/
+void behing(BTtree p)
+{
+    if(p != NULL)
+    {
+        behing(p -> lchild);
+        behing(p -> rchild);
+        //前驱搜索
+        if(p -> lchild == NULL)
+        {
+            pre -> rtag = true;
+            pre -> lchild = pre;
+        }
+        //后驱搜索
+        if(pre -> rchild == NULL)
+        {
+            pre -> rtag = true;
+            pre ->  rchild  = p
+        }
+        pre = p;
+    }
+}
+/*
+*后序线索化二叉树的起始函数
+*/
+BTtree toTreeListBeh(BTtree &BT)
+{
+    BTtree beh;
+    beh = new BTtree;
+    beh -> ltag = false;
+    beh -> rtag = true;
+    beh -> rchild = beh;
+    if(BT == NULL)
+    {
+        beh -> lchild = beh;
+    }
+    else
+    {
+        beh -> lchild = BT;
+        pre = beh;
+        behing(BT);
+        beh -> rchild = pre;
+    }
+    return beh;
+}
+BTtree Parent(BTtree & HEAD, BTtree & p)
+{
+    BTtree tep;
+    tep = HEAD;
+    //tep是头节点
+    if(tep -> lchild == p)
+    {
+        return tep;
+    }
+    else
+    {
+        tep = tep -> lchild;
+        while(tep -> lchild != p && tep -> rchild != p)
+        {
+            if(tep -> rtag == false)
+            {
+                //有右节点，往右走
+                tep = tep -> rchild;
+            }
+            else
+            {
+                //左儿子，到前驱
+                tep = tep -> lchild;
+            }
+        }
+        return tep;
+    }
+}
+/*
+*后序线索化二叉树的输出函数
+*/
+void toTreeListBehPrint(BTtree HEAD)
+{
+    BTtree p, parent;
+    p = HEAD -> lchild;
+    while(true)
+    {
+        while(p -> ltag == 0)
+            p = p -> lchild;
+        if(p -> rtag == 0)
+            p = p -> rchild;
+        else
+            break;
+    }
+    while(HEAD != p)
+    {
+        cout << p -> data << "->";
+        parent = Parent(HEAD, P);
+        //如果parent是HEAD,则p是根节点,没有后继
+        if(HEAD == parent)
+            p = HEAD;
+
+        else if(parent -> rchild == p || parent -> rtag == true)
+        {
+            p = parent;
+        }
+        //如果p是双亲的右孩子，或者是独生左孩子，，则后继为双亲
+        else
+        {
+            while(parent -> rtag == false)
+            {
+                //若P是有兄弟的左孩子，则后继为双亲的右子树上后序遍历访问的第一个节点
+                parent = parent -> rchild;
+                while(parent -> ltag == false)
+                {
+                    parent = parent -> lchild;
+                }
+            }
+            p = parent;
+        }
+        cout << p -> data << endl;
+    }
+}
+void resetTreeList(BTtree HEAD, BTtree &BT)
+{
+    BTtree p, post;
+    p = HEAD -> lchild;
+    while(p != HEAD)
+    {
+        while(p -> ltag == false)
+            p = p -> lchild;
+        p -> ltag  = false;
+        p -> lchild  = NULL;
+        while(p -> rtag == true && p -> rchild != HEAD)
+        {
+            p -> rtag  = false;
+            post = p -> rchild;
+            p -> rchild = NULL;
+            p = post;
+        }
+        p = p -> rchild;;
+    }
+    p = HEAD -> rchild;
+    p -> rtag = false;
+    p -> rchild = NULL;
+    BT = HEAD -> lchild;
+    delete(HEAD);
+}
 void toTreeList(BTtree * BT)
 {
     //头节点
     BTtree * HEAD;
+    cout << "==前序==" << endl;
+    toTreeListPre(BT, HEAD);
+    cout << "遍历前序线索化二叉树：" << endl;
+    toTreeListPrePrint(HEAD);
     cout << "==中序==" << endl;
     toTreeListMid(BT, HEAD);
-    cout << "遍历中序线索化二叉树" << endl;
+    cout << "遍历中序线索化二叉树：" << endl;
     toTreeListMidPrint(HEAD);
-
+    cout << "==后序==" << endl;
+    BTtree beh;
+    //会改变BT
+    beh = toTreeListBehPrint(BT);
+    cout <<　"遍历后序线索化二叉树：" << endl;
+    toTreeListBehPrint(beh);
 }
+//主函数
 int main()
 {
     BTtree *pre_tree = NULL;
